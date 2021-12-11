24e6f4f8dfd437cf4a562c6f8d94943b93d7b7ed
Pluto130 <89603351+Pluto130@users.noreply.github.com>
2021-12-11 03:50:29 -0500

    part 1- iffy

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,30 +1,50 @@
 extends Area2D
 
-var VP := Vector2.ZERO
-var velocity := Vector2(0,-10)
-
-var Explosions = null
-var Explosion = preload("res://Explosion/Explosion.tscn")
+var VP = null
+var velocity = Vector2.ZERO
+var accel = 0.4
 
 func _ready():
 	VP = get_viewport().size
 
 func _physics_process(_delta):
 	position += velocity.rotated(rotation)
-	position.x = wrapf(position.x,0,VP.x)
-	position.y = wrapf(position.y,0,VP.y)
-
-func _on_Timer_timeout():
-	queue_free()
-
+	velocity.y -= accel
+	if position.x > VP.x or position.x < 0 or position.y > VP.y or position.y < 0:
+		queue_free()
 
 func _on_Bullet_body_entered(body):
-	if Explosions == null:
-		Explosions = get_node_or_null("/root/Game/Explosions")
-	if Explosions != null:
-		var explosion = Explosion.instance()
-		explosion.position = position
-		Explosions.add_child(explosion)
 	if body.has_method("die"):
 		body.die()
 	queue_free()
+	
+##########################################################################
+
+#var VP := Vector2.ZERO
+#var velocity := Vector2(0,-10)
+
+#var Explosions = null
+#var Explosion = preload("res://Explosion/Explosion.tscn")
+
+#func _ready():
+#	VP = get_viewport().size
+
+#func _physics_process(_delta):
+#	position += velocity.rotated(rotation)
+#	position.x = wrapf(position.x,0,VP.x)
+#	position.y = wrapf(position.y,0,VP.y)
+
+#func _on_Timer_timeout():
+#	queue_free()
+
+
+#func _on_Bullet_body_entered(body):
+#	if Explosions == null:
+#		Explosions = get_node_or_null("/root/Game/Explosions")
+#	if Explosions != null:
+#		var explosion = Explosion.instance()
+#		explosion.position = position
+#		Explosions.add_child(explosion)
+#	if body.has_method("die"):
+#		body.die()
+#	queue_free()
